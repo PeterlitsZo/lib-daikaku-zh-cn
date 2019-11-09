97f3a70d1b936175c62156befc6e0138f69abd48
Peterlits Zo <peterlitszo@outlook.com>
2019-11-09 20:13:34 +0800

    add a part of py programme

--- makedoc.py
+++ makedoc.py
@@ -1,6 +1,8 @@
-def ltmd2latex(s:str):
+import re
+
+def ltmd2latex(s:str) -> str:
     """
-    input will like:
+    input will like:(or something else)
     |   $1+1=$
     |   -------------------------
     |   - $2$
@@ -9,6 +11,7 @@ def ltmd2latex(s:str):
     |   - there is no corrent anwser
     and then output will be:
     |   $1+1=$
+    |   \hspace*{\fill}\mbox{(~~~~~~~~~~)}
     |   \begin{enumerate}[A.]
     |       \item $2$
     |       \item $\lim_{x\to 0} f(x)=x$
@@ -16,4 +19,43 @@ def ltmd2latex(s:str):
     |       \item there is no corrent anwser
     |   \end{enumerate}
     """
-    
+    str_begin_with_sym_sub = r'-+[^-\s]+|-+[^-]+.+'
+    str_begin_without_sym_sub = r'[^-].*'
+    # <str> ::= <str_begin_with_sym_sub> <newline> <title>
+    #        |  <str_begin_without_sym_sub> <newline> <title>
+    #        |  ...
+    # <title> ::= <str>
+
+    line = r'-+\s*'
+
+    item_begin = r'-\s.*'
+    # <item> ::= <item_brgin> <newline> <str>
+    #         |  <item_begin>
+
+    spaceline = r'\s*'
+
+    # <valid> ::= <title>
+    #          |  <title> <newline> <line>
+    #          |  <title> <newline> <line> <item>+
+    #          |  <title> <newline> <item>+
+
+    def ltmd2token(s:str) -> list:
+        """
+        this is a sub function of ltmd2latex
+        """
+        result_list = []
+        for i in s.split('\n'):
+            if re.fullmatch(line, i)
+                result_list.append(('line', i))
+            elif re.fullmatch(str_begin_with_sym_sub, i):
+                result_list.append(('str_begin_with_sym_sub', i))
+            elif re.fullmatch(str_begin_without_sym_sub, i):
+                result_list.append(('str_begin_without_sym_sub', i))
+            elif re.fullmatch(spaceline, i):
+                result_list.append(('spaceline', i))
+            elif re.fullmatch(item_begin, i):
+                result_list.append(('item_begin', i))
+            else:
+                result_list.append(('invaild', i))
+
+    return ltmd2token(s)
